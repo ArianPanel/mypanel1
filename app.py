56248061cd1b531554f1bb868c4687abb1363a0d
ArianPanel <arianaarian349@gmail.com>
2026-08-27 00:27:45 +0330

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,5 @@ def create_config():
 def logout():
     session.clear()
     return redirect(url_for('login'))
-
-if 
    if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080))) 

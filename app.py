db255d88778d57654e063eda9809213c145ae955
ArianPanel <arianaarian349@gmail.com>
2026-08-27 00:20:30 +0330

    Update app.py

--- app.py
+++ app.py
@@ -106,4 +106,5 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+   if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080))) 
